428018f012e8a29380ae1bbba136aff8d72ce333
mansouralsaleh <alsaleh.mansour@gmail.com>
2016-01-18 16:18:50 -0500

    This is for the tutorial on getting started with java on Heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,8 +26,11 @@ public class Main {
 
     get("/hello", (req, res) -> {
       RelativisticModel.select();
-      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-      return "E=mc^2: 12 GeV = " + m.toString();
+
+      String energy = System.getenv().get("ENERGY");
+
+      Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+      return "E=mc^2: " + energy + " = " + m.toString();
     });
 
     get("/", (request, response) -> {
