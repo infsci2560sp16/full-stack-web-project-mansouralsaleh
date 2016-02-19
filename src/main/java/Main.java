a9177810ed5a49998d65ba71a95f5d234108625d
mansouralsaleh <alsaleh.mansour@gmail.com>
2016-02-19 01:25:24 -0500

    edited main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,7 +26,7 @@ public class Main {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "../../../public/main.html");
+            return new ModelAndView(attributes, "index.html");
         }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
