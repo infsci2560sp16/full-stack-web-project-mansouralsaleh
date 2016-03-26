90f0e998bdc564f3f8ed5674f17122b5721c3b22
mansouralsaleh <alsaleh.mansour@gmail.com>
2016-03-26 00:03:36 -0400

    Fixed second service

--- src/main/java/Services/BooksService.java
+++ src/main/java/Services/BooksService.java
@@ -110,7 +110,7 @@ public class BooksService {
     
     public ArrayList<Map<String, String>> getOneBook(String id){
         arrayList = new ArrayList<>();
-        int pointer;
+        int pointer = 3;
         if(id !=""){
             switch(id){
                 case "0001":
