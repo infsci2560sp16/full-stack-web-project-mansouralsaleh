dd6522d025f283bc4ec36d4331d4ac9189feea02
mansouralsaleh <alsaleh.mansour@gmail.com>
2016-03-31 01:56:41 -0400

    recovering from fetal mistake

--- src/main/java/Services/Books/BooksService.java
+++ src/main/java/Services/Books/BooksService.java
@@ -237,7 +237,7 @@ public class BooksService {
 	
 	
     public static String getOneUniversities(String id){
-		String output = null;
+		String output ;
 	try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
