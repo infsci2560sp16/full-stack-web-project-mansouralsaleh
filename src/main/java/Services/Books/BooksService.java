e59d1f2c8dd83abf574bf7913afa11da3b3cae76
mansouralsaleh <alsaleh.mansour@gmail.com>
2016-03-31 02:38:58 -0400

    recovering from fetal mistake

--- src/main/java/Services/Books/BooksService.java
+++ src/main/java/Services/Books/BooksService.java
@@ -238,12 +238,12 @@ public class BooksService {
 	
     public static String getOneUniversities(String id){
 		String output = null;
-	try {output="hahaha";
+	try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setNamespaceAware(true);
-            Document doc = factory.newDocumentBuilder().parse(new URL("src/main/resources/public/unixml.xml").openStream());
+            Document doc = factory.newDocumentBuilder().parse(new URL("https://nameless-mountain-5787.herokuapp.com/unixml.xml").openStream());
             doc.getDocumentElement().normalize();
             NodeList nodes = doc.getElementsByTagName("University");
             for (int i = 0; i < nodes.getLength(); i++) {
