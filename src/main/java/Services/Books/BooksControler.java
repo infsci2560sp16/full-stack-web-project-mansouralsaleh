56977f92cf9d11112e9841f9ade10bd1c8e872ee
mansouralsaleh <alsaleh.mansour@gmail.com>
2016-04-06 18:34:36 -0400

    added database

--- src/main/java/Services/Books/BooksControler.java
+++ src/main/java/Services/Books/BooksControler.java
@@ -1,6 +1,11 @@
 package Services.Books;
 
 import com.google.gson.Gson;
+import com.heroku.sdk.jdbc.DatabaseUrl;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import static spark.Spark.*;
@@ -103,6 +108,32 @@ public class BooksControler {
              aboutpart.put("aboutpart", about);
             return new ModelAndView(aboutpart, "registration.ftl");
         }, new FreeMarkerEngine());
+        
+        get("/db", (req, res) -> {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try {
+        connection = DatabaseUrl.extract().getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+        ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+        }
+
+        attributes.put("results", output);
+        return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+      }
+    }, new FreeMarkerEngine());
     
     }
 
